9124ce68b9da4bff762e581ddddaf3e684927e1d
Henrik Andreasson <henrik.andreasson@cgi.com>
2022-11-19 11:36:35 +0100

    repo create + sign

--- bin/apt-repo-custom-source-download-and-create-repo.sh
+++ bin/apt-repo-custom-source-download-and-create-repo.sh
@@ -73,3 +73,12 @@ for pkg in ${PACKAGES} ; do
 	  done
 	fi
 done
+
+
+rm -rf Packages Packages.gz Release Release.gpg InRelease
+
+apt-ftparchive --arch amd64 packages . > Packages
+gzip -k -f Packages
+apt-ftparchive release . > Release
+gpg  -abs -o Release.gpg Release
+gpg --clearsign -o InRelease Release
